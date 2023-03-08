c43347b291a5184c5c88593d1334d1bad705f085
Елена <elenammmmm@gmail.com>
2023-03-08 23:01:15 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Variables2();
     }
+    public static void Variables2() {
+        byte yorkshireTerrier = 120;
+        System.out.println("Значение переменной yorkshireTerrier с типом byte равно "+ yorkshireTerrier);
+        short frenchBulldog = -30000;
+        System.out.println("Значение переменной frenchBulldog с типом short равно "+ frenchBulldog);
+        int spaniel = 987654321;
+        System.out.println("Значение переменной spaniel с типом int равно "+ spaniel);
+        long dachshund = 1234567891234567890L;
+        System.out.println("Значение переменной dachshund с типом long равно "+ dachshund);
+    }
+
+
 }
