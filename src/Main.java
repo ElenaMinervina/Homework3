14128c24c67be541fabf071341bfb23d64a7dc6e
Елена <elenammmmm@gmail.com>
2023-03-09 21:28:25 +0300

    Исправила имена переменных в задачах 4 и 5. Начала задачу 6.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         Variables2_3();
         Variables2_4();
         Variables2_5();
+        Variables2_6();
     }
     public static void Variables2_1() {
         byte yorkshireTerrier = 120;
@@ -37,24 +38,27 @@ public class Main {
     }
     public static void Variables2_4() {
         byte time = 2;
-        byte quantityInTime = 16;
-        int speedInMinute = quantityInTime / time;
-        int quantity20 = speedInMinute * 20;
-        System.out.println("За 20 минут машина произвела " + quantity20 + " бутылок");
-        int quantityDay = speedInMinute * 60 * 24;
-        System.out.println("За сутки машина произвела " + quantityDay + " бутылок");
-        int quantity3Days = quantityDay * 3;
-        System.out.println("За 3 дня машина произвела " + quantity3Days + " бутылок");
-        int quantityMonth  = quantityDay * 30;
-        System.out.println("За месяц машина произвела " + quantityMonth + " бутылок");
+        byte numberInTime = 16;
+        int speedInMinute = numberInTime / time;
+        int number20 = speedInMinute * 20;
+        System.out.println("За 20 минут машина произвела " + number20 + " бутылок");
+        int numberDay = speedInMinute * 60 * 24;
+        System.out.println("За сутки машина произвела " + numberDay + " бутылок");
+        int number3Days = numberDay * 3;
+        System.out.println("За 3 дня машина произвела " + number3Days + " бутылок");
+        int numberMonth  = numberDay * 30;
+        System.out.println("За месяц машина произвела " + numberMonth + " бутылок");
     }
     public static void Variables2_5() {
         byte whiteAndBrown = 120;
         byte oneClassWhite = 2;
         byte oneClassBrown = 4;
-        int quantityClasses = whiteAndBrown / (oneClassWhite + oneClassBrown);
-        int white = quantityClasses * oneClassWhite;
-        int brown = quantityClasses * oneClassBrown;
-        System.out.println("В школе, где " + quantityClasses + " классов, нужно " + white + " банок белой краски и " + brown + " банок коричневой краски");
+        int numberClasses = whiteAndBrown / (oneClassWhite + oneClassBrown);
+        int white = numberClasses * oneClassWhite;
+        int brown = numberClasses * oneClassBrown;
+        System.out.println("В школе, где " + numberClasses + " классов, нужно " + white + " банок белой краски и " + brown + " банок коричневой краски");
+    }
+    public static void Variables2_6() {
+
     }
 }
