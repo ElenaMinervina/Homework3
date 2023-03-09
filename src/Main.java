2bc515582e4f5eb3e78144091a7cac429a368d63
Елена <elenammmmm@gmail.com>
2023-03-09 20:38:04 +0300

    Изменения в задаче 3. Задача 4 - начало.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Variables2_1();
         Variables2_2();
+        Variables2_3();
+        Variables2_4();
     }
     public static void Variables2_1() {
         byte yorkshireTerrier = 120;
@@ -15,13 +17,25 @@ public class Main {
     }
     public static void Variables2_2() {
         float a = 27.2f;
-        long b = 987678965549l;
+        long b = 987678965549L;
         float c = 2.786f;
         short d = 569;
         short e = -159;
         short f = 27897;
         byte g = 67;
+        long h = (long) (a+b+c+d+e+f+g);
+        System.out.println(h);
     }
+    public static void Variables2_3() {
+        byte ludmilaPavlovnasStudents = 23;
+        byte annaSergeevnasStudents = 27;
+        byte ekaterinaAndreevnasStudents = 30;
+        short totalSheets = 480;
+        int eachStudentSheets = totalSheets / (ludmilaPavlovnasStudents + annaSergeevnasStudents + ekaterinaAndreevnasStudents);
+        System.out.println("На каждого ученика рассчитано " + eachStudentSheets + " листов бумаги");
+    }
+    public static void Variables2_4() {
 
+    }
 
 }
